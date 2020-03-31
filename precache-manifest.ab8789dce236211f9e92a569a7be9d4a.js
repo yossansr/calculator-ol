45cc372ab5997f76862abe598d1d1b76c84d9132
yossansr <yossansr06@gmail.com>
2020-03-31 23:30:23 +0800

    Updates

---
+++ precache-manifest.ab8789dce236211f9e92a569a7be9d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c60e0a5f7ffbb353f2845c003140ec",
+    "url": "/calculator-ol/index.html"
+  },
+  {
+    "revision": "d5244b3aef2933278a2a",
+    "url": "/calculator-ol/static/css/main.b13525f3.chunk.css"
+  },
+  {
+    "revision": "5bbdf37ddd71acfd6ef2",
+    "url": "/calculator-ol/static/js/2.1be762e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-ol/static/js/2.1be762e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5244b3aef2933278a2a",
+    "url": "/calculator-ol/static/js/main.fadfca48.chunk.js"
+  },
+  {
+    "revision": "910fd2177063d22c1b41",
+    "url": "/calculator-ol/static/js/runtime-main.ec9b00fa.js"
+  }
+]);
